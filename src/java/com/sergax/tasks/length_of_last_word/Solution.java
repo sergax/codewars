38b38c8db717f2f0cbfc64bb8e9873978fd1eba0
sergax <serey.aksenchenko@gmail.com>
2022-05-16 00:16:45 +0300

    add solution length of last word

---
+++ src/java/com/sergax/tasks/length_of_last_word/Solution.java
@@ -0,0 +1,16 @@
+package com.sergax.tasks.length_of_last_word;
+
+/**
+ * by Aksenchenko Serhii on 15.05.2022
+ */
+
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        int index = 0;
+        String[] array = s.split("\\s+");
+        for (int i = 0; i < array.length; i++) {
+            index = array[array.length - 1].length();
+        }
+        return index;
+    }
+}
