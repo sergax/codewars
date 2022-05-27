e88a1d396e8eb833c726fdcf3947ba0c492e89b2
sergax <sergey.aksenchenko@gmail.com>
2022-05-27 13:38:09 +0300

    add solution for Remove duplicates from list

---
+++ src/java/com/sergax/tasks/remove_duplicates_from_list/ListNode.java
@@ -0,0 +1,14 @@
+package com.sergax.tasks.remove_duplicates_from_list;
+
+/**
+ * @author : sergax
+ * @date : 27.05.22
+ */
+
+public class ListNode {
+     int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
