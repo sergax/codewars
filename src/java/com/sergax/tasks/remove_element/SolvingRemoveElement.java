2255d9b1e2e9f1478085aea2ccf95ca2b8418749
sergax <sergey.aksenchenko@gmail.com>
2022-06-27 00:31:51 +0300

    add Solution .)

--- src/java/com/sergax/tasks/remove_element/SolvingRemoveElement.java
+++ src/java/com/sergax/tasks/remove_element/SolvingRemoveElement.java
@@ -1,15 +1,13 @@
 package com.sergax.tasks.remove_element;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
+
 
 /***
  * Solving :
  * [(1),2,5,(1),8,7,(1)] duplicate = 1
  * as result = [2,5,8,7, , , ]
- * return count = 4
  */
 
 public class SolvingRemoveElement {
@@ -22,10 +20,11 @@ public class SolvingRemoveElement {
                 newMap.add(num);
         }
         return newMap;
+
     }
 
     public static void main(String[] args) {
         SolvingRemoveElement s = new SolvingRemoveElement();
-        System.out.println(s.removeElement(new int[]{1, 2, 3, 4, 1}, 1));
+        System.out.println(s.removeElement(new int[]{1, 5, 2, 1, 8, 7, 1}, 1));
     }
 }
