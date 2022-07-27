15f1838fef3bbcd6b19168d0155499b589abd1f3
sergax <sergey.aksenchenko@gmail.com>
2022-07-27 22:37:19 +0300

    add new Solution

---
+++ src/java/com/sergax/tasks/tree_node/TreeNode.java
@@ -0,0 +1,43 @@
+package com.sergax.tasks.tree_node;
+
+/**
+ * @author: sergax
+ * @date: 27.07.22
+ * <p>
+ * How deep is tree
+ * 4 == 3
+ * / \
+ * 10   23 == 2
+ * / \
+ * 12   6 == 1
+ * \
+ * null == 0
+ * <p>
+ * response: 3
+ */
+public class TreeNode {
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int value) {
+        this.value = value;
+    }
+}
+
+class TreeDepthFinder {
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(13);
+        findDepthOfTree(treeNode);
+    }
+
+    static int findDepthOfTree(TreeNode treeNode) {
+        if (treeNode == null) return 0;
+
+        int leftMaxDepth = findDepthOfTree(treeNode.left);
+        int rightMaxDepth = findDepthOfTree(treeNode.right);
+        int max = Math.max(leftMaxDepth, rightMaxDepth);
+        System.out.println(max);
+        return max;
+    }
+}
