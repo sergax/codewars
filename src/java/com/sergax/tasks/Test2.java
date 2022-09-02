be3dceef718472ce62c21ff56b0be0ac4cf8a7a1
user <sergey.aksenchenko@rumpl.io>
2022-09-02 11:29:35 +0300

    add Solution

--- src/java/com/sergax/tasks/Test2.java
+++ src/java/com/sergax/tasks/Test2.java
@@ -1,12 +1,15 @@
 package com.sergax.tasks;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Test2 {
 
     public static void main(String[] args) {
         double[] numbers = new double[]{-2.398};
-
+        String localPrefix = new String("5").intern();
+        String s = "5";
+        if (localPrefix == s) System.out.println("+");
         double sum = Arrays.stream(numbers).sum();
         System.out.println(sum);
     }
