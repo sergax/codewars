1b50103186bdc5337e82afbc4a3203e9f176bc5a
sergax <sergey.aksenchenko@gmail.com>
2022-09-21 17:57:00 +0300

    add Solution for Sum of two

---
+++ src/java/com/sergax/tasks/kyu6/pile_of_Cubes/sum_of_two/Sum.java
@@ -0,0 +1,41 @@
+package com.sergax.tasks.kyu6.pile_of_Cubes.sum_of_two;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * [1, 2, 5, 3] -> 6 -> [0,2]
+ * target - array[i]
+ */
+public class Sum {
+    public static int[] method(int[] array, int target) throws IllegalAccessException {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            int next = target - array[i];
+            if (map.containsKey(next)) {
+                return new int[]{map.get(next), i};
+            }
+            map.put(array[i], i);
+        }
+        throw new IllegalAccessException("Not found such elements");
+    }
+
+    @Test
+    public void test1() throws IllegalAccessException {
+        assertArrayEquals(new int[]{0, 1}, Sum.method(new int[]{1, 2, 5, 3, 10, 1}, 3));
+    }
+
+    @Test
+    public void test2() throws IllegalAccessException {
+        assertArrayEquals(new int[]{2, 4}, Sum.method(new int[]{1, 2, 5, 3, 10, 1}, 15));
+    }
+
+    @Test
+    public void test3() throws IllegalAccessException {
+        assertArrayEquals(new int[]{0, 5}, Sum.method(new int[]{1, 2, 5, 3, 10, 1}, 2));
+    }
+}
